cbe20043630dab3647e3a70b522859e1599c927c
gotaheads <gotahiroki@gmail.com>
2016-01-26 22:58:28 +1100

    style header and quota summary

---
+++ app/features/quotes/view/view.js
@@ -0,0 +1,9 @@
+function ViewQuoteCtrl($scope, QuoteService) {
+  QuoteService.findOrCreateForView($scope);
+
+  $scope.fromNow =function(date) {
+    return moment(date).fromNow();
+  }
+}
+ViewQuoteCtrl.$inject = ['$scope', 'QuoteService'];
+
--- app/js/quotes/view.js
+++
@@ -1,5 +0,0 @@
-function ViewQuoteCtrl($scope, QuoteService) {
-    QuoteService.findOrCreateForView($scope);
-}
-ViewQuoteCtrl.$inject = ['$scope', 'QuoteService'];
-
