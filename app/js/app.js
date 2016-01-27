73ad443ce4dcbd1cab9ac9b812b31303ebf3fa9a
gotaheads <gotahiroki@gmail.com>
2016-01-27 13:55:07 +1100

    works done

--- app/js/app.js
+++ app/js/app.js
@@ -57,7 +57,7 @@ var myApp = angular.module('jobq', ['$strap.directives', 'ui.bootstrap', 'ngGrid
     {templateUrl: 'partials/print-contract.html', controller: ViewContractCtrl});
     $routeProvider.when('/view-contract/:quoteId', {templateUrl: 'partials/view-contract.html', controller: ViewContractCtrl});
     
-    $routeProvider.when('/edit-works/:quoteId', {templateUrl: 'partials/edit-works.html', controller: EditWorksCtrl});
+    $routeProvider.when('/edit-works/:quoteId', {templateUrl: 'features/quotes/works/works.html', controller: EditWorksCtrl});
     $routeProvider.when('/edit-quote/:quoteId/:workIdx', {templateUrl: 'partials/edit-quote.html', controller: EditQuoteCtrl});
     $routeProvider.when('/edit-labours/:quoteId/:workIdx', {templateUrl: 'partials/edit-labours.html', controller: EditLabourCtrl});
 
