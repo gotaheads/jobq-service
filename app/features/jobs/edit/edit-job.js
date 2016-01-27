47c8b1aa422c6c891a854b65c4de5a8f44a46afe
gotaheads <gotahiroki@gmail.com>
2016-01-27 14:54:59 +1100

    format

--- app/features/jobs/edit/edit-job.js
+++ app/features/jobs/edit/edit-job.js
@@ -1,101 +1,101 @@
-
 function EditJobCtrl($scope, JobService, $http, $routeParams, QuoteService) {
-    var $log = $scope.$log;
-    $scope.title = 'Edit job';
-    var jobId = $scope.jobId = $routeParams.jobId;
-    $scope.finalised = false;
-    $scope.statuses = ['Preparing Quote', 'Quote approved',
-        'Work in progress'
-        ,'Completed', 'Cancelled'];
+  var $log = $scope.$log;
+  $scope.title = 'Edit job';
+  var jobId = $scope.jobId = $routeParams.jobId;
+  $scope.finalised = false;
+  $scope.statuses = ['Preparing Quote', 'Quote approved',
+    'Work in progress'
+    , 'Completed', 'Cancelled'];
 
-    $scope.$log.info("EditJobCtrl " + $scope.jobId);
+  $scope.$log.info("EditJobCtrl " + $scope.jobId);
 
-    var url = createUrl('/jobs/' + $scope.jobId);
+  var url = createUrl('/jobs/' + $scope.jobId);
 
-    JobService.load(jobId).then(function(job) {
-        $scope.job = job;
-        $scope.quote = job.quote;
-        $scope.editing = job;
+  JobService.load(jobId).then(function (job) {
+    $scope.job = job;
+    $scope.quote = job.quote;
+    $scope.editing = job;
 
 
-        var workIdx = 0;
-        //$scope.quote = {};
-        $scope.workIdx = workIdx;
+    var workIdx = 0;
+    //$scope.quote = {};
+    $scope.workIdx = workIdx;
 
-        switch(job.status) {
-            case 'Completed':
-            case 'Cancelled':
-                $scope.finalised = true;
-        }
+    switch (job.status) {
+      case 'Completed':
+      case 'Cancelled':
+        $scope.finalised = true;
+    }
 
-        $scope.$watch("editing.client", function(value) {
-            console.log("client changed: " + value);
-            $scope.quote.client = value;
-        },true);
-        $scope.$watch("editing.address", function(value) {
-            console.log("address changed: " + value);
-            $scope.quote.address = value;
-        },true);
-        $scope.$watch("editing.budget", function(value) {
-            console.log("Budget changed: " + value);
-            $scope.quote.budget = value;
-        },true);
+    $scope.$watch("editing.client", function (value) {
+      console.log("client changed: " + value);
+      $scope.quote.client = value;
+    }, true);
+    $scope.$watch("editing.address", function (value) {
+      console.log("address changed: " + value);
+      $scope.quote.address = value;
+    }, true);
+    $scope.$watch("editing.budget", function (value) {
+      console.log("Budget changed: " + value);
+      $scope.quote.budget = value;
+    }, true);
 
-        JobService.updateActions($scope);
+    JobService.updateActions($scope);
 
-        $scope.addRecentJob(job._id, job.client);
+    $scope.addRecentJob(job._id, job.client);
+  });
+
+  $scope.printQuote = function () {
+    $log.info("EditJobCtrl printQuote : " + $scope.quote._id);
+    QuoteService.openPrint($scope.quote);
+  }
+
+  $scope.printJob = function () {
+    var jobId = $scope.job._id;
+    $log.info("EditJobCtrl printJob: " + jobId);
+
+    JobService.saveJob($scope.job).then(function (result) {
+      var job = result.data;
+      $scope.$log.info("saveJob finished " + jobId);
+
+      window.open(
+        '#/print-job/' + jobId,
+        '_blank'
+      );
     });
+  }
 
-    $scope.printQuote = function() {
-        $log.info("EditJobCtrl printQuote : " + $scope.quote._id);
-        QuoteService.openPrint($scope.quote);
-    }
+  $scope.cancel = function () {
+    $scope.job.status = 'Cancelled';
+    $scope.save();
+  }
 
-    $scope.printJob = function() {
-        var jobId = $scope.job._id;
-        $log.info("EditJobCtrl printJob: " + jobId);
+  $scope.complete = function () {
+    $scope.job.status = 'Completed';
+    $scope.save();
+  }
 
-        JobService.saveJob($scope.job).then(function(result) {
-            var job = result.data;
-            $scope.$log.info("saveJob finished " + jobId);
-            window.open(
-                '#/print-job/' + jobId,
-                '_blank'
-            );
-        });
-    }
+  $scope.save = function () {
+    var job = $scope.job;
 
-    $scope.cancel = function() {
-        $scope.job.status = 'Cancelled';
-        $scope.save();
-    }
+    JobService.saveJob(job).then(function (result) {
+      $scope.$log.info("saveJob finished " + result.data);
 
-    $scope.complete = function() {
-        $scope.job.status = 'Completed';
-        $scope.save();
-    }
+      QuoteService.updateStatus($scope.quote, job);
+      QuoteService.updateQuoteStatus(job);
+    });
+  };
 
-    $scope.save = function() {
-        var job = $scope.job;
+  $scope.remove = function () {
+    JobService.remove($scope.job).then(function (result) {
+      $scope.$log.info("remove finished " + result.data);
+      $scope.location.path('/dashboard');
+    });
+  };
 
-        JobService.saveJob(job).then(function(result) {
-            $scope.$log.info("saveJob finished " + result.data);
-
-            QuoteService.updateStatus($scope.quote, job);
-            QuoteService.updateQuoteStatus(job);
-        });
-    };
-
-    $scope.remove = function() {
-        JobService.remove($scope.job).then(function(result) {
-            $scope.$log.info("remove finished " + result.data);
-            $scope.location.path('/dashboard');
-        });
-    };
-
-    $scope.$watch("editing.works", function(value) {
-        console.log("Model: " + value);
-    },true);
+  $scope.$watch("editing.works", function (value) {
+    console.log("Model: " + value);
+  }, true);
 }
 
-EditJobCtrl.$inject = ['$scope', 'JobService', '$http', '$routeParams','QuoteService'];
+EditJobCtrl.$inject = ['$scope', 'JobService', '$http', '$routeParams', 'QuoteService'];
